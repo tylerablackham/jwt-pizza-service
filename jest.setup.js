2082475a4b085cccb2a5688580c62f46c9698ca7
Tyler Blackham <tylerablackham@gmail.com>
2025-09-22 19:13:11 -0600

    feat: attach linter and testing infrastructure to project

---
+++ jest.setup.js
@@ -0,0 +1,3 @@
+if (process.env.DEBUG) {
+    jest.setTimeout(Infinity);
+}
---
+++ src/util/testHelper.js
@@ -0,0 +1,19 @@
+const { Role, DB } = require('../database/database.js');
+
+function randomName() {
+    return Math.random().toString(36).substring(2, 12);
+}
+
+async function createAdminUser() {
+    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+    user.name = randomName();
+    user.email = user.name + '@admin.com';
+
+    user = await DB.addUser(user);
+    return { ...user, password: 'toomanysecrets' };
+}
+
+exports = {
+    randomName,
+    createAdminUser
+}
