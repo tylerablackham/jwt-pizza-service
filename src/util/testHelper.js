0913d99161c437e8d0782ccdc085c6d690931341
Tyler Blackham <tylerablackham@gmail.com>
2025-09-26 17:58:58 -0600

    feat: move tests to test directory

--- src/util/testHelper.js
+++ src/util/testHelper.js
@@ -4,16 +4,19 @@ function randomName() {
     return Math.random().toString(36).substring(2, 12);
 }
 
+const TEST_ADMIN_PASSWORD = 'ThisIsNotSecret'
+
 async function createAdminUser() {
-    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+    let user = { password: TEST_ADMIN_PASSWORD, roles: [{ role: Role.Admin }] };
     user.name = randomName();
     user.email = user.name + '@admin.com';
 
     user = await DB.addUser(user);
-    return { ...user, password: 'toomanysecrets' };
+    return { ...user, password: TEST_ADMIN_PASSWORD };
 }
 
 exports = {
     randomName,
-    createAdminUser
+    createAdminUser,
+    TEST_ADMIN_PASSWORD
 }
--- test/routes/authRouter.test.js
+++ test/routes/authRouter.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../service');
+const app = require('../../src/service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
