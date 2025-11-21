4bd90cf156f1798df2dfa1524c10c2ce5a7f2c60
Tyler Blackham <tylerablackham@gmail.com>
2025-11-21 16:22:17 -0700

    fix: undo commenting out logic in mock traffic script

--- src/scripts/mockTraffic.sh
+++ src/scripts/mockTraffic.sh
@@ -28,28 +28,26 @@ while true; do
   response3=$(curl -X PUT $host/api/auth -d "{\"email\":\"$name@jwt.com\", \"password\":\"pass\"}" -H 'Content-Type: application/json')
   token2=$(echo $response3 | jq -r '.token')
   curl -X DELETE $host/api/auth -H "Authorization: Bearer $token2" > /dev/null
-  #curl -X GET $host/api/crash > /dev/null
+  curl -X GET $host/api/crash > /dev/null
   sleep $((RANDOM % 2 + 1))
 done &
 pid1=$!
 
-# order too many pizzas
+# update greeting
 while true; do
   response4=$(curl -X PUT $host/api/auth -d "{\"email\":\"a@jwt.com\", \"password\":\"admin\"}" -H 'Content-Type: application/json')
   token3=$(echo $response4 | jq -r '.token')
-  sleep 30
-  curl -X DELETE $host/api/auth -H "Authorization: Bearer $token3" > /dev/null
   sleep 10
-  #items=$(jq -n '[range(21) | {"menuId":1, "description":"Veggie", "price":0.0038}]')
-  #curl -X POST $host/api/order -d "{\"franchiseId\": 1, \"storeId\":1, \"items\":$items}" -H 'Content-Type: application/json' -H "Authorization: Bearer $token3"
+  items=$(jq -n '[range(21) | {"menuId":1, "description":"Veggie", "price":0.0038}]')
+  curl -X POST $host/api/order -d "{\"franchiseId\": 1, \"storeId\":1, \"items\":$items}" -H 'Content-Type: application/json' -H "Authorization: Bearer $token3"
 
   sleep $((RANDOM % 9 + 2))
 done &
 pid2=$!
 
-# wrong sign in
+# reset greeting
 while true; do
-  #curl -X PUT $host/api/auth -d "{\"email\":\"a@jwt.com\", \"password\":\"wrong\"}" -H 'Content-Type: application/json' > /dev/null
+  curl -X PUT $host/api/auth -d "{\"email\":\"a@jwt.com\", \"password\":\"wrong\"}" -H 'Content-Type: application/json' > /dev/null
   sleep 10
   sleep $((RANDOM % 10 + 1))
 done &
